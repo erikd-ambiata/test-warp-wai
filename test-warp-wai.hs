b720b64dd4a507186caa9589c5caff4f45100e1f
Erik de Castro Lopo <erik.decastrolopo@ambiata.com>
2017-02-08 12:29:19 +1100

    test-warp-wai.hs: Simplify
    Specifically want this to die as soon as it encounters an error.

--- test-warp-wai.hs
+++ test-warp-wai.hs
@@ -9,7 +9,4 @@ main :: IO ()
 main = do
     void . forkIO $ runHttpsClient httpsPort
     putStrLn "HTTPS client thread started."
-    -- Let the HTTPS client run for a while before starting the HTTP server.
-    threadDelay (15 * 1000 * 1000)
-    void . forkIO $ runHttpServer httpPort
-    forever $ threadDelay (1000 * 1000)
+    runHttpServer httpPort
